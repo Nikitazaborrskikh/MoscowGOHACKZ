2646534ef3091fe0e5e4d4c89322e1c76497c45f
Nikitazaborrskikh <73338822+Nikitazaborrskikh@users.noreply.github.com>
2024-09-26 13:19:39 +0300

    Update CameraFollow.cs

--- MoscowGO/Assets/Scripts/CameraFollow.cs
+++ MoscowGO/Assets/Scripts/CameraFollow.cs
@@ -7,12 +7,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Transform _target;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
     void LateUpdate()
     {
         Vector3 desiredPosition = _target.position + offset;
